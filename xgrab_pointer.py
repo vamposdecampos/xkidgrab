24e5bcfb20281bb281946cc036eee16ffcfb9f52
Alex Badea <vamposdecampos@gmail.com>
2012-12-29 15:34:48 +0200

    Change the grab cursor to a pirate glyph
    There doesn't seem to be an XCreateFontCursor in python-xlib, so we
    hand-code one.

--- xgrab_pointer.py
+++ xgrab_pointer.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python
 
 from Xlib.display import Display
-from Xlib import X, XK
+from Xlib import X, XK, Xcursorfont
+
+def create_font_cursor(dpy, cursor_idx):
+	cursor_font = dpy.open_font('cursor')
+	return cursor_font.create_glyph_cursor(cursor_font,
+		cursor_idx, cursor_idx + 1,
+		# swapped bg/fg colors
+		(0xffff, 0xffff, 0xffff), (0, 0, 0))
 
 dpy = Display()
 root = dpy.screen().root
 
+cursor = create_font_cursor(dpy, Xcursorfont.pirate)
 
 print "# grab:", root.grab_pointer(False, X.ButtonPressMask | X.ButtonReleaseMask,
-	X.GrabModeSync, X.GrabModeAsync, X.NONE, X.NONE, X.CurrentTime)
+	X.GrabModeSync, X.GrabModeAsync, X.NONE, cursor, X.CurrentTime)
 
 try:
 	while True:
