b3ed57c50ea27865fd1f881c3b74857cbbb773c8
Alex Badea <vamposdecampos@gmail.com>
2012-12-29 15:52:00 +0200

    Also grab the keyboard
    Release with Escape.

--- xgrab_pointer.py
+++ xgrab_pointer.py
@@ -14,18 +14,24 @@ dpy = Display()
 root = dpy.screen().root
 
 cursor = create_font_cursor(dpy, Xcursorfont.pirate)
+esc_keycode = dpy.keysym_to_keycode(XK.XK_Escape)
 
-print "# grab:", root.grab_pointer(False, X.ButtonPressMask | X.ButtonReleaseMask,
-	X.GrabModeSync, X.GrabModeAsync, X.NONE, cursor, X.CurrentTime)
+print "# grab pointer:", root.grab_pointer(False,
+	X.ButtonPressMask | X.ButtonReleaseMask,
+	X.GrabModeAsync, X.GrabModeAsync, X.NONE, cursor, X.CurrentTime)
+print "# grab keyboard:", root.grab_keyboard(False,
+	X.GrabModeAsync, X.GrabModeAsync, X.CurrentTime)
 
 try:
 	while True:
-		dpy.allow_events(X.SyncPointer, X.CurrentTime)
 		ev = root.display.next_event()
 		print "event:", ev
-		
-	pass
+
+		if ev.type == X.KeyRelease and ev.detail == esc_keycode:
+			print "# escape"
+			break
 finally:
+	dpy.ungrab_keyboard(X.CurrentTime)
 	dpy.ungrab_pointer(X.CurrentTime)
 
 print "# done"
